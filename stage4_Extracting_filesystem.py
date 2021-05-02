50372e46b5f35606c2912c7ffb7cea02aabdb72f
mrTavas <d.tavasiev1997@gmail.com>
2021-05-03 01:31:33 +0300

    update manual extract

--- stage4_Extracting_filesystem.py
+++ stage4_Extracting_filesystem.py
@@ -9,18 +9,23 @@ def Stage4_Extracting_filesystem(firmware, filesystemType):
     # Try extract filesystem
     try:
         subprocess.check_call('binwsalk -e ' + firmware, shell=True)
-        
+
+    # Extract error   
     except:
         
         # Wrile Filesystem address if config file
-        subprocess.check_call('binwalk ' + firmware + ' | grep -i filesystem | awk \'{print $2}\' >> config.conf', shell=True)
+        subprocess.check_call('binwalk ' + firmware + 
+        ' | grep -i filesystem | awk \'{print $2}\' >> config.conf', shell=True)
 
         # Get Filesystem address from config file
         with open("config.conf", "r") as f:
             addressFilesystem = str(f.readlines()[3]).rstrip()
         
         # Put filesystem in manual mode
-        subprocess.check_call('dd if=' + firmware + ' bs=1 skip=$((' + addressFilesystem + ')) of=dir.' + filesystemType, shell=True)
+        subprocess.check_call('dd if=' + firmware + 
+        ' bs=1 skip=$((' + addressFilesystem + ')) of=dir.' 
+        + filesystemType, shell=True)
+        
         print(filesystemType)
     
         # Unpaking filesystem in manual mode
